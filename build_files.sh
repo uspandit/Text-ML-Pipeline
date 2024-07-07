dca6ceca090be08ac0b8889fa1afa77b836ddb86
uspandit <utkarsh.sharma_cs.da21@gla.ac.in>
2024-07-07 14:21:55 +0530

    Prepare for Vercel deployment with virtual environment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y libpq-dev
+
+# Create and activate the virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Install Python dependencies
 python3 -m pip install -r requirements.txt
 
